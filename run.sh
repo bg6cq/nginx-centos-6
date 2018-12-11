6b078bd5ce4d72d046cd20dca7d0d6a262c62ec5
Zhang Huanjie <james@ustc.edu.cn>
2018-12-11 14:08:56 +0800

    run.sh

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+rpm -i https://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+yum -y install git gcc rpm-build rpmdevtools geoip-devel gd-devel pcre-devel
+yum -y install perl-devel perl-ExtUtils-Embed libxslt-devel
+yum update
+
+cd /root
+git clone https://git.ustc.edu.cn/james/centos-nginx rpmbuild
+
+cd /root/rpmbuild
+rpmbuild -bb SPECS/LuaJIT.spec
+
+rpm -i RPMS/x86_64/LuaJIT-2.0.5-1.el6.x86_64.rpm
+
+cd /root/rpmbuild
+rpmbuild -ba SPECS/nginx.spec
+
